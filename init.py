d9af45e5c27b122e6543a54683fc716aa31a0af2
zhangguowei <285009003@qq.com>
2020-07-17 14:45:06 +0800

    Initial commit

---
+++ init.py
@@ -0,0 +1,30 @@
+import socket
+import fcntl
+import struct
+
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15].encode("utf-8"))
+    )[20:24])
+
+with open('IP','w') as wf:
+    wf.write(get_ip_address('eth0'))
+f = open('IP', 'r')
+print(f.read())
+
+
+
+"""
+def read_file(filepath):
+    with open(filepath) as fp:
+        content = fp.read();
+    return content
+
+
+ret = read_file('IP')
+print(ret)
+"""
