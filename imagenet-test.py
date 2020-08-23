5545cd7e711f6b5cc896b0e10929bdacea127dd6
zhangguowei <285009003@qq.com>
2020-08-24 04:18:20 +0800

    Correct

--- imagenet-test.py
+++ imagenet-test.py
@@ -38,8 +38,6 @@ def main():
                         help='mini-batch size (default: 256), this is the total '
                              'batch size of all GPUs on the current node when '
                              'using Data Parallel or Distributed Data Parallel')
-    parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
-                        help='number of data loading workers (default: 4)')
     parser.add_argument('--epochs', default=2, type=int, metavar='N',
                         help='number of total epochs to run')
     parser.add_argument('--dist-backend',  default='nccl', type=str,
@@ -52,28 +50,6 @@ def main():
     print('master:', os.environ['MASTER_ADDR'], 'port:', os.environ['MASTER_PORT'])
     mp.spawn(train, nprocs=args.gpus, args=(args,))
 
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-
-
-
 def train(gpu, args):
     print("start train")
     rank = int(os.environ['PAI_TASK_INDEX']) * args.gpus + gpu
@@ -82,7 +58,7 @@ def train(gpu, args):
     model=model = models.__dict__[args.arch]()
     torch.cuda.set_device(gpu)
     model.cuda(gpu)
-    batch_size = 100
+    batch_size = args.batch_size
     # define loss function (criterion) and optimizer
     criterion = nn.CrossEntropyLoss().cuda(gpu)
     optimizer = torch.optim.SGD(model.parameters(), 1e-4)
@@ -117,44 +93,6 @@ def train(gpu, args):
         ])),
         batch_size=args.batch_size, shuffle=False,
         num_workers=args.nodes, pin_memory=True)
-    '''
-    # Data loading code
-    transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
-
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
-
-    trainset = torchvision.datasets.CIFAR10(
-        root='./data', train=True, download=True, transform=transform_train)
-    # sampler 进行shuffle，参考https://pytorch.org/docs/stable/data.html#torch.utils.data.distributed.DistributedSampler
-    # 每个process应该是不同的数据切片
-    # 对于DP数据切片是自动的，DDP要自己手动做数据切片
-    # 训练分片应该按照world size和rank来切片
-
-    trainsampler = torch.utils.data.distributed.DistributedSampler(
-        trainset,
-        num_replicas=args.world_size,
-        rank=rank,
-        shuffle=True,
-    )
-    trainloader = torch.utils.data.DataLoader(
-        trainset, batch_size=batch_size, shuffle=False, num_workers=2, sampler=trainsampler)
-
-    testset = torchvision.datasets.CIFAR10(
-        root='./data', train=False, download=True, transform=transform_test)
-    testloader = torch.utils.data.DataLoader(
-        testset, batch_size=batch_size, shuffle=False, num_workers=0, pin_memory=True, sampler=trainsampler)
-
-    classes = ('plane', 'car', 'bird', 'cat', 'deer',
-               'dog', 'frog', 'horse', 'ship', 'truck')
-    '''
     start = datetime.now()
     total_step = len(train_loader)
     for epoch in range(args.epochs):
